52d2771d223aa2371733839f801bc8265b2c9b77
nibinbs <bsnibin@gmail.com>
2023-05-29 13:04:13 +0800

    add instance

--- instance.tf
+++ instance.tf
@@ -1,4 +1,4 @@
-/*resource "aws_instance" "git_ins" {
+resource "aws_instance" "git_ins" {
   ami           = "ami-0126086c4e272d3c9"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.git_sub.id
@@ -7,4 +7,4 @@
         Name  = "ec21_${lookup(var.tags,"name")}"
         Email = lookup(var.tags, "email")
     }
-}*/
+}
