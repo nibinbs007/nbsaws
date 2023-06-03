f638842a4390d8c23f57b3809eba884feb84b7b7
nibinbs <bsnibin@gmail.com>
2023-06-03 17:06:02 +0800

    Creatre windows vm

--- instance.tf
+++ instance.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "cmt1-win" {
   key_name      = "git-key"
   subnet_id     = aws_subnet.cmt1-sub1.id
   vpc_security_group_ids = [aws_security_group.cmt1-sg1.id]
+  associate_public_ip_address = true
   tags = {
     Name  = "cmt1-win"
     Email = "nibin.bahulayansheena@ncs.com.sg"
