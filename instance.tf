9e640b2ba90a73cc5a2ad2aff59f9740896293d8
Nibin <bsnibin@gmail.com>
2023-08-31 16:16:28 +0800

    add linux

--- instance.tf
+++ instance.tf
@@ -12,3 +12,19 @@ resource "aws_instance" "cmt1-win" {
     Env   = "Dev"
   }
 }
+
+#Create Linux VM
+resource "aws_instance" "cmt1-app" {
+  ami                         = "ami-0464f90f5928bccb8"
+  instance_type               = "t2.micro"
+  key_name                    = "git-key"
+  subnet_id                   = aws_subnet.cmt1-sub1.id
+  vpc_security_group_ids      = [aws_security_group.cmt1-sg2.id]
+  associate_public_ip_address = true
+  count = 2
+  tags = {
+   name = "cmt1-app${count.index}"
+    Email = var.email
+    Env   = "Dev"
+  }  
+}
