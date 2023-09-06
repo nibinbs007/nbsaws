2368af646dba4e317ca4a484c90ed2d135c9b472
Nibin <bsnibin@gmail.com>
2023-09-06 09:53:03 +0800

    add redhat vm

--- instance.tf
+++ instance.tf
@@ -28,3 +28,18 @@ resource "aws_instance" "cmt1-app" {
     Env   = "Dev"
   }  
 }
+
+#Create Redhat ansible node
+resource "aws_instance" "cmt1-app" {
+  ami                         = "ami-02acda7aaa1f944e5"
+  instance_type               = "t2.micro"
+  key_name                    = "git-key"
+  subnet_id                   = aws_subnet.cmt1-sub1.id
+  vpc_security_group_ids      = [aws_security_group.cmt1-sg1.id]
+  associate_public_ip_address = true
+  tags = {
+    Name = "Ansible"
+    Email = var.email
+    Env   = "Dev"
+  }  
+}
