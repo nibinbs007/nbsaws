6479d1caa7c485fe15a2ea0ec0eeb7a2ab866819
Nibin <bsnibin@gmail.com>
2023-10-07 01:03:00 +0800

    change to red hat

--- instance.tf
+++ instance.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "cmt1-win" {
   }
 }
 
-#Create Linux VM
+/*#Create Linux VM
 resource "aws_instance" "cmt1-app" {
   ami                         = "ami-0464f90f5928bccb8"
   instance_type               = "t2.micro"
@@ -28,6 +28,7 @@ resource "aws_instance" "cmt1-app" {
     Env   = "Dev"
   }  
 }
+*/
 
 #Create Redhat Ansible node
 resource "aws_instance" "Ansible" {
@@ -37,6 +38,22 @@ resource "aws_instance" "Ansible" {
   subnet_id                   = aws_subnet.cmt1-sub1.id
   vpc_security_group_ids      = [aws_security_group.cmt1-sg1.id]
   associate_public_ip_address = true
+  tags = {
+    Name = "Ansible"
+    Email = var.email
+    Env   = "Dev"
+  }  
+}
+
+#Create redhat workloads
+resource "aws_instance" "Ansible" {
+  ami                         = "ami-02acda7aaa1f944e5"
+  instance_type               = "t2.micro"
+  key_name                    = "git-key"
+  subnet_id                   = aws_subnet.cmt1-sub1.id
+  vpc_security_group_ids      = [aws_security_group.cmt1-sg1.id]
+  associate_public_ip_address = true
+  count = 2
   tags = {
     Name = "Ansible"
     Email = var.email
