c4851131790a4554dd94ace0e58bb4730f463fae
Nibin <bsnibin@gmail.com>
2023-11-25 02:46:43 +0800

    update pipeline

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
-/*terraform {
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-}*/
+}
 
 #Configure provider and access
 provider "aws" {
-  version = "~> 4.0"
   region  = var.region # Singapore region
 }
